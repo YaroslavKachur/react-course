5a78f0a861dfed5f3d886a3a9352ac1f3e66bbf2
YaroslavKachur <yaroslav.kachur.2009@gmail.com>
2026-04-19 14:54:24 +0200

    add handleToggleDone

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,39 +2,38 @@ import { useState } from 'react'
 import './App.css'
 
 function MessageList() {
-  const [text, setText] = useState("");
-  const [messages, setMessages] = useState([]);
+const [text, setText] = useState("");
+const [messages, setMessages] = useState([]);
 
-  function handleAdd() {
-    if(text === "") return;
+function handleAdd() {
+  if(text === "") return;
 
-    const newMessage = {
-      id : Date.now(),
-      text: text,
-    }; 
-    
-    setMessages((prev) => [...prev, newMessage])
-    setText("");
+  const newMessage = {
+    id: Date.now(),
+    text: text,
+    done: false,
+  };
+  setMessages((prev) => [...prev, newMessage]);
+  setText("");
+}
+
+function handleToggleDone(idToToggle) {
+  setMessages((prev) => prev.map((msg) => msg.id === idToToggle ? {...msg, done: !msg.done} : msg));
+}
+
+  function handleDelete(idToDelete){
+    setMessages((prev) => prev.filter((msg) => msg.id !== idToDelete) );
   }
 
-  function handleDelete(idToDelete) {
-    setMessages((prev) => prev.filter((msg) => msg.id !== idToDelete))
-  }
-  
   return(
     <div>
-      <input 
-      type="text" value={text} placeholder='Write here' 
+      <input type="text" value={text} placeholder='Write here' 
       onChange={(e) => setText(e.target.value)} />
-
-    <button 
-    onClick={handleAdd} style={{borderRadius:"10px", border:"1px solid"}}>
-      Add
-    </button>
-    {messages.map((msg) => (<p key={msg.id}>{msg.text} <button onClick={() => handleDelete(msg.id)}>Delete</button></p>))}
+      <button onClick={handleAdd}>Add</button>
+      {messages.map(
+        (msg) => <p key={msg.id}>{msg.text} <button onClick={() => handleToggleDone(msg.id)}>{msg.done ? "Undo" : "Do"}</button> <button onClick={() => handleDelete(msg.id)}>X</button></p>)}
     </div>
   )
-
 }
 
 
