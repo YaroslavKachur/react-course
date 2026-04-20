4064212a83724d51dba3cb086a6445b73f6ebf32
YaroslavKachur <yaroslav.kachur.2009@gmail.com>
2026-04-20 22:26:48 +0200

    add edit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 function MessageList() {
 const [text, setText] = useState("");
 const [messages, setMessages] = useState([]);
+const [editedText, setEditedText] = useState("");
+const [editingId, setEditingdId] = useState(null);
 
 function handleAdd() {
   if(text === "") return;
@@ -26,22 +28,64 @@ function handleToggleDone(idToDone) {
   setMessages((prev) => prev.filter((msg) => msg.id !== idToDelete))
  }
 
+ function handleStartEdit(id, currentText) {
+  setEditingdId(id);
+  setEditedText(currentText);
+ }
+
+ function handleSaveEdit(idToSave) {
+  setMessages((prev) => prev.map((msg) => msg.id === idToSave ? {...msg, text: editedText} : msg));
+
+  setEditingdId(null);
+  setEditedText("");
+ }
+
   return(
     <div>
-      <input type="text" value={text} placeholder='write' 
-      onChange={(e) => setText(e.target.value)} />
-      <button onClick={handleAdd}>
-        Add
-      </button>
-      {messages.map((msg) => 
-        (<p key={msg.id}> 
-        {msg.text}     {msg.done ? <span style={{color:"green"}}>Done</span> : <span style={{color:"red"}}>Not done</span>}        <br />
-        <button onClick={
-          () => handleToggleDone(msg.id)}>
-            {msg.done ? "Undo" : "Do"}</button> 
-            <button onClick={() => handleDelete(msg.id)}>Delete</button></p>))}
+      <input
+        type="text"
+        value={text}
+        placeholder="write"
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button onClick={handleAdd}>Add</button>
+
+      {messages.map((msg) => (
+        <div key={msg.id}>
+          {editingId === msg.id ? (
+            <>
+              <input
+                type="text"
+                value={editedText}
+                onChange={(e) => setEditedText(e.target.value)}
+              />
+              <button onClick={() => handleSaveEdit(msg.id)}>
+                Save
+              </button>
+            </>
+          ) : (
+            <>
+              {msg.text}{" "}
+              <span style={{ color: msg.done ? "green" : "red" }}>
+                {msg.done ? "Done" : "Not done"}
+              </span>
+              <br />
+              <button onClick={() => handleToggleDone(msg.id)}>
+                {msg.done ? "Undo" : "Do"}
+              </button>
+              <button onClick={() => handleStartEdit(msg.id, msg.text)}>
+                Edit
+              </button>
+              <button onClick={() => handleDelete(msg.id)}>
+                Delete
+              </button>
+            </>
+          )}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 
