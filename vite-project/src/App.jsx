9ea13a27fb922b78729a89823e699b3e498332d6
YaroslavKachur <yaroslav.kachur.2009@gmail.com>
2026-04-20 20:52:15 +0200

    add handleToggleDone

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,30 +8,38 @@ const [messages, setMessages] = useState([]);
 function handleAdd() {
   if(text === "") return;
 
-  const newMessage = {
-    id: Date.now(),
-    text: text,
-    done: false,
-  };
+    const newMessage = {
+      id: Date.now(),
+      text: text,
+      done: false,
+    }
   setMessages((prev) => [...prev, newMessage]);
   setText("");
 }
 
-function handleToggleDone(idToToggle) {
-  setMessages((prev) => prev.map((msg) => msg.id === idToToggle ? {...msg, done: !msg.done} : msg));
+function handleToggleDone(idToDone) {
+  setMessages((prev) => prev.map((msg) => msg.id === idToDone ? { ...msg, done: !msg.done } : msg ));
 }
 
-  function handleDelete(idToDelete){
-    setMessages((prev) => prev.filter((msg) => msg.id !== idToDelete) );
-  }
+
+ function handleDelete(idToDelete) {
+  setMessages((prev) => prev.filter((msg) => msg.id !== idToDelete))
+ }
 
   return(
     <div>
-      <input type="text" value={text} placeholder='Write here' 
+      <input type="text" value={text} placeholder='write' 
       onChange={(e) => setText(e.target.value)} />
-      <button onClick={handleAdd}>Add</button>
-      {messages.map(
-        (msg) => <p key={msg.id}>{msg.text} <button onClick={() => handleToggleDone(msg.id)}>{msg.done ? "Undo" : "Do"}</button> <button onClick={() => handleDelete(msg.id)}>X</button></p>)}
+      <button onClick={handleAdd}>
+        Add
+      </button>
+      {messages.map((msg) => 
+        (<p key={msg.id}> 
+        {msg.text}     {msg.done ? <span style={{color:"green"}}>Done</span> : <span style={{color:"red"}}>Not done</span>}        <br />
+        <button onClick={
+          () => handleToggleDone(msg.id)}>
+            {msg.done ? "Undo" : "Do"}</button> 
+            <button onClick={() => handleDelete(msg.id)}>Delete</button></p>))}
     </div>
   )
 }
